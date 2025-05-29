ca0fc06f1c7e07eb5695122d8f8a8cf870b11303
Bogdan Popa <bogdan@defn.io>
2025-05-29 20:31:15 +0300

    core: rename actor thread to `who'

--- actor-lib/main.rkt
+++ actor-lib/main.rkt
@@ -68,62 +68,64 @@
   (define ch (make-channel))
   (define thd
     (thread/suspend-to-kill
-     (lambda ()
-       (let loop ([actor-st (actor-state null (make-state))])
-         (define st
-           (actor-state-state actor-st))
-         (define stopped?
-           (stopped?-proc st))
-         (define receive?
-           (and (not stopped?)
-                (receive?-proc st)))
-         (cond
-           [(and stopped? (null? (actor-state-reqs actor-st)))
-            (on-stop-proc st)]
-           [else
-            (loop
-             (with-handlers ([exn:fail?
-                              (lambda (e)
-                                (begin0 actor-st
-                                  ((error-display-handler)
-                                   (format "~a: ~a" who (exn-message e))
-                                   e)))])
-               (apply
-                sync
-                (handle-evt
-                 (if receive? ch never-evt)
-                 (match-lambda
-                   [`(,id ,res-ch ,nack-evt . ,args)
-                    (define-values (next-st res)
-                      (with-handlers ([exn:fail?
-                                       (lambda (e)
-                                         (values st e))])
-                        (method-proc st id args)))
-                    (&actor-state-state
-                     (lens-update
-                      &actor-state-reqs actor-st
-                      (λ (reqs) (cons (req res res-ch nack-evt) reqs)))
-                     next-st)]
-                   [message
-                    (begin0 actor-st
-                      (log-actor-error "~a: invalid message ~.s" who message))]))
-                (handle-evt
-                 (if stopped? never-evt (make-event st))
-                 (lambda (next-st)
-                   (&actor-state-state actor-st next-st)))
-                (append
-                 (for/list ([r (in-list (actor-state-reqs actor-st))])
-                   (handle-evt
-                    (req-nack-evt r)
-                    (lambda (_)
-                      (lens-update &actor-state-reqs actor-st (λ (reqs) (remq r reqs))))))
-                 (for/list ([r (in-list (actor-state-reqs actor-st))])
-                   (handle-evt
-                    (channel-put-evt
-                     (req-res-ch r)
-                     (req-res r))
-                    (lambda (_)
-                      (lens-update &actor-state-reqs actor-st (λ (reqs) (remq r reqs))))))))))])))))
+     (procedure-rename
+      (lambda ()
+        (let loop ([actor-st (actor-state null (make-state))])
+          (define st
+            (actor-state-state actor-st))
+          (define stopped?
+            (stopped?-proc st))
+          (define receive?
+            (and (not stopped?)
+                 (receive?-proc st)))
+          (cond
+            [(and stopped? (null? (actor-state-reqs actor-st)))
+             (on-stop-proc st)]
+            [else
+             (loop
+              (with-handlers ([exn:fail?
+                               (lambda (e)
+                                 (begin0 actor-st
+                                   ((error-display-handler)
+                                    (format "~a: ~a" who (exn-message e))
+                                    e)))])
+                (apply
+                 sync
+                 (handle-evt
+                  (if receive? ch never-evt)
+                  (match-lambda
+                    [`(,id ,res-ch ,nack-evt . ,args)
+                     (define-values (next-st res)
+                       (with-handlers ([exn:fail?
+                                        (lambda (e)
+                                          (values st e))])
+                         (method-proc st id args)))
+                     (&actor-state-state
+                      (lens-update
+                       &actor-state-reqs actor-st
+                       (λ (reqs) (cons (req res res-ch nack-evt) reqs)))
+                      next-st)]
+                    [message
+                     (begin0 actor-st
+                       (log-actor-error "~a: invalid message ~.s" who message))]))
+                 (handle-evt
+                  (if stopped? never-evt (make-event st))
+                  (lambda (next-st)
+                    (&actor-state-state actor-st next-st)))
+                 (append
+                  (for/list ([r (in-list (actor-state-reqs actor-st))])
+                    (handle-evt
+                     (req-nack-evt r)
+                     (lambda (_)
+                       (lens-update &actor-state-reqs actor-st (λ (reqs) (remq r reqs))))))
+                  (for/list ([r (in-list (actor-state-reqs actor-st))])
+                    (handle-evt
+                     (channel-put-evt
+                      (req-res-ch r)
+                      (req-res r))
+                     (lambda (_)
+                       (lens-update &actor-state-reqs actor-st (λ (reqs) (remq r reqs))))))))))])))
+      who)))
   (actor who ch thd))
 
 (define (actor-evt a id . args)
